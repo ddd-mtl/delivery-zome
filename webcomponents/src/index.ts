0be860945faac2118ed6bb73631c0701d877cec8
damien <doute.d@gmail.com>
2023-09-13 09:27:56 -0400

    Forgot to export perspective

--- webcomponents/src/index.ts
+++ webcomponents/src/index.ts
@@ -2,6 +2,7 @@
 export * from './bindings/delivery.types';
 export * from './bindings/delivery.proxy';
 
+export * from './viewModels/delivery.perspective';
 export * from './viewModels/delivery.zvm';
 
 export * from './elements/delivery-dashboard';
