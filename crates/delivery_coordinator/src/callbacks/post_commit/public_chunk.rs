b23689c65fa4814ede6e21c0c718dad8afa24dc7
damien <doute.d@gmail.com>
2023-10-09 16:23:00 -0400

    Added signal for public chunk

--- crates/delivery_coordinator/src/callbacks/post_commit/public_chunk.rs
+++ crates/delivery_coordinator/src/callbacks/post_commit/public_chunk.rs
@@ -1,11 +1,17 @@
 use hdk::prelude::*;
 use zome_delivery_types::*;
+use crate::SignalProtocol;
 
 
 ///
 pub fn post_commit_PublicChunk(_sah: &SignedActionHashed, entry: Entry, chunk_eh: &EntryHash) -> ExternResult<()> {
    debug!("post_commit_PublicChunk() {:?}", chunk_eh);
-   let _ = ParcelChunk::try_from(entry)?;
+   let chunk = ParcelChunk::try_from(entry)?;
+   /// Emit signal
+   let res = emit_signal(&SignalProtocol::NewLocalChunk((chunk_eh.to_owned(), chunk.clone())));
+   if let Err(err) = res {
+      error!("Emit signal failed: {}", err);
+   }
    /// Done
    Ok(())
 }
