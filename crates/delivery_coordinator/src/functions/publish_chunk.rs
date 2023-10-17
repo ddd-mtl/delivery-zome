d09ed74b910f1a9fb88e984e48a460ea26d0af30
damien <doute.d@gmail.com>
2023-10-17 13:29:52 -0400

    Implemented publish_chunks()

--- crates/delivery_coordinator/src/functions/publish_chunk.rs
+++ crates/delivery_coordinator/src/functions/publish_chunk.rs
@@ -4,12 +4,26 @@ use zome_utils::*;
 use zome_delivery_types::*;
 use zome_delivery_integrity::*;
 
+/// Batch call to publish_chunk()
+/// WARN: Make sure input does not exceed websocket packet max size limit.
+#[hdk_extern]
+pub fn publish_chunks(chunks: Vec<ParcelChunk>) -> ExternResult<Vec<EntryHash>> {
+   std::panic::set_hook(Box::new(zome_panic_hook));
+   debug!("chunks count: {}", chunks.len());
+   let mut result = Vec::new();
+   for chunk in chunks {
+      let eh = publish_chunk(chunk)?;
+      result.push(eh);
+   }
+   Ok(result)
+}
+
+
 /// Write base64 data as string to source chain
 /// Return EntryHash of newly created ParcelChunk
 #[hdk_extern]
 pub fn publish_chunk(chunk: ParcelChunk) -> ExternResult<EntryHash> {
    trace!("chunk size: {} bytes", chunk.data.len());
-   std::panic::set_hook(Box::new(zome_panic_hook));
    /// Commit entry
    //let chunk = ParcelChunk {data};
    let chunk_eh = hash_entry(chunk.clone())?;
