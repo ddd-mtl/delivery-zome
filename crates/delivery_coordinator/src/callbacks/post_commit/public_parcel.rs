3491bb2eda4b6f3928d93e79d980e81edab7e437
damien <doute.d@gmail.com>
2024-06-10 12:25:35 -0400

    cleanup

--- crates/delivery_coordinator/src/callbacks/post_commit/public_parcel.rs
+++ crates/delivery_coordinator/src/callbacks/post_commit/public_parcel.rs
@@ -8,19 +8,9 @@ use crate::{DeliveryGossip, emit_gossip_signal};
 pub fn post_commit_create_PublicParcel(_sah: &SignedActionHashed, create: &Create, entry: Entry) -> ExternResult<DeliveryEntryKind> {
    debug!("post_commit_create_PublicParcel() create: {}", create.entry_hash);
    let parcel_reference = ParcelReference::try_from(entry)?;
-   //debug!("post_commit_create_PublicParcel() pr_eh = {}", hash_entry(parcel_reference.clone()).unwrap());
    /// Create Anchor
    let response = call_self("link_public_parcel", create.entry_hash.clone())?;
    let _ah = decode_response::<ActionHash>(response)?;
-   // /// Emit Signal
-   // let dg = DeliveryGossip {
-   //   from: agent_info()?.agent_latest_pubkey,
-   //   gossip: DeliveryGossipProtocol::PublicParcelPublished((eh.to_owned(), sah.hashed.content.timestamp(), parcel_reference)),
-   // };
-   // let res = emit_gossip_signal(dg);
-   // if let Err(err) = res {
-   //    error!("Emit signal failed: {}", err);
-   // }
    /// Done
    Ok(DeliveryEntryKind::PublicParcel(parcel_reference))
 }
@@ -38,7 +28,7 @@ pub fn gossip_public_parcel(create_link: &CreateLink, ts: Timestamp, isCreate: b
       return;
    }
    let Some(pr_record) = maybe.unwrap()
-     else { error!("ParcelReference record not found"); return };
+     else { error!("ParcelReference record not found. It might be the initial anchor link."); return };
    let Ok(pr) = get_typed_from_record::<ParcelReference>(pr_record)
      else { error!("Failed to convert entry to ParcelReference"); return };
    /// Emit Signal
