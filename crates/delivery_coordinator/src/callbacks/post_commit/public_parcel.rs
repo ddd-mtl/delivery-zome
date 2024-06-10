a0fc78d66643a18cb990750d056276eea122cc7c
damien <doute.d@gmail.com>
2024-06-10 12:04:01 -0400

    more error logs

--- crates/delivery_coordinator/src/callbacks/post_commit/public_parcel.rs
+++ crates/delivery_coordinator/src/callbacks/post_commit/public_parcel.rs
@@ -6,8 +6,9 @@ use crate::{DeliveryGossip, emit_gossip_signal};
 
 ///
 pub fn post_commit_create_PublicParcel(_sah: &SignedActionHashed, create: &Create, entry: Entry) -> ExternResult<DeliveryEntryKind> {
-   debug!("post_commit_create_PublicParcel() {:?}", create.entry_hash);
+   debug!("post_commit_create_PublicParcel() create: {}", create.entry_hash);
    let parcel_reference = ParcelReference::try_from(entry)?;
+   debug!("post_commit_create_PublicParcel() pr_eh = {}", hash_entry(parcel_reference.clone()).unwrap());
    /// Create Anchor
    let response = call_self("link_public_parcel", create.entry_hash.clone())?;
    let _ah = decode_response::<ActionHash>(response)?;
@@ -28,19 +29,24 @@ pub fn post_commit_create_PublicParcel(_sah: &SignedActionHashed, create: &Creat
 ///
 pub fn gossip_public_parcel(create_link: &CreateLink, ts: Timestamp, isCreate: bool) {
    /// Get ParcelReference
-   let pr_Eh = EntryHash::try_from(create_link.target_address.clone()).unwrap();
-   debug!("gossip_public_parcel({}) {}", isCreate, pr_Eh);
-   let Ok(pr_record) = get_local_from_eh(pr_Eh.clone())
-     else { error!("Failed to get ParcelReference record"); return };
+   let pr_eh = EntryHash::try_from(create_link.target_address.clone()).unwrap();
+   debug!("gossip_public_parcel({}) {}", isCreate, pr_eh);
+   let maybe = get(pr_eh.clone(), GetOptions::local());
+   if let Err(e) = maybe {
+      error!("Failed to get ParcelReference record: {:?}", e);
+      return;
+   }
+   let Some(pr_record) = maybe.unwrap()
+     else { error!("ParcelReference record not found"); return };
    let Ok(pr) = get_typed_from_record::<ParcelReference>(pr_record)
-     else { error!("Failed to get ParcelReference"); return };
+     else { error!("Failed to convert entry to ParcelReference"); return };
    /// Emit Signal
    let dg = DeliveryGossip {
       from: agent_info().unwrap().agent_latest_pubkey,
       gossip: if isCreate {
-         DeliveryGossipProtocol::PublicParcelPublished((pr_Eh, ts, pr))
+         DeliveryGossipProtocol::PublicParcelPublished((pr_eh, ts, pr))
       } else {
-         DeliveryGossipProtocol::PublicParcelUnpublished((pr_Eh, ts, pr))
+         DeliveryGossipProtocol::PublicParcelUnpublished((pr_eh, ts, pr))
       },
    };
    let res = emit_gossip_signal(dg);
