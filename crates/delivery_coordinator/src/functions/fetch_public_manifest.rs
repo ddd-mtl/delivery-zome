00516642f5f39cc440d5eb5a101e5291ae040865
damien <doute.d@gmail.com>
2024-06-27 11:45:40 -0400

    emit entry signal in fetch_chunk()

--- crates/delivery_coordinator/src/functions/fetch_public_manifest.rs
+++ crates/delivery_coordinator/src/functions/fetch_public_manifest.rs
@@ -18,6 +18,10 @@ pub fn fetch_public_manifest(manifest_eh: EntryHash) -> ExternResult<(ParcelMani
 ///
 #[hdk_extern]
 pub fn fetch_chunk(chunk_eh: EntryHash) -> ExternResult<ParcelChunk> {
-   let chunk: ParcelChunk = get_typed_from_eh(chunk_eh)?;
+   //let chunk: ParcelChunk = get_typed_from_eh(chunk_eh)?;
+   let (record, chunk) = get_typed_and_record::<ParcelChunk>(&AnyLinkableHash::from(chunk_eh))?;
+   /// Emit signal
+   emit_new_entry_signal(record.clone(), false)?;
+   ///
    Ok(chunk)
 }
